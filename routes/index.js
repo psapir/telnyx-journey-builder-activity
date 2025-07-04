7cfa1e8bb1457f30d523f62ae33108f10cdad70d
Pato Sapir <patriciosapir@gmail.com>
2025-07-04 09:59:21 -0400

    logging

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,11 @@ export async function txml(req, res) {
     const message = req.query.message || "Hello, this is a test call.";
     const language = req.query.language || "en-US";
     const voice = req.query.voice || "female"; // 'female' or 'male'
-
+    console.log(`
+        <Response>
+            <SpeakSentence language="${language}" voice="${voice}">${message}</SpeakSentence>
+        </Response>
+    `);
     res.type('text/xml');
     res.send(`
         <Response>
